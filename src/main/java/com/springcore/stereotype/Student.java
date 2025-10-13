82592fdfb55f897a8dcdd015df2b807992b45e24
Rohit Narayani <rohitnarayani2005@gmail.com>
2025-10-13 11:51:53 +0530

    bean scope(singletone,prototype)

--- src/main/java/com/springcore/stereotype/Student.java
+++ src/main/java/com/springcore/stereotype/Student.java
@@ -1,14 +1,28 @@
 package com.springcore.stereotype;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("obj")
+@Scope("prototype")
 public class Student {
 	@Value("Rohit")
 	private String name;
 	@Value("30")
 	private int rollno;
+	
+	@Value("#{temp}")
+	private List<String> friends;
+	
+	public List<String> getFriends() {
+		return friends;
+	}
+	public void setFriends(List<String> friends) {
+		this.friends = friends;
+	}
 	public String getName() {
 		return name;
 	}
--- src/main/java/com/springcore/stereotype/Test.java
+++ src/main/java/com/springcore/stereotype/Test.java
@@ -9,8 +9,29 @@ public class Test {
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/stereotype/stereoconfig.xml");
 		
-		Student student = context.getBean("obj",Student.class);
-		System.out.println(student);
+		Student student1 = context.getBean("obj",Student.class);
+		Student student2 = context.getBean("obj",Student.class);
+		
+//		System.out.println(student);
+//		System.out.println(student.getFriends());
+//		System.out.println(student.getFriends().getClass().getName());
+		
+		Sample sample1 = context.getBean("s1",Sample.class);
+		Sample sample2 = context.getBean("s1",Sample.class);
+		
+		System.out.println(sample1.hashCode());
+		
+		System.out.println("__________________________________________________________________");
+		
+		System.out.println(sample2.hashCode());
+		
+		System.out.println("__________________________________________________________________");
+		
+		System.out.println(student1.hashCode());
+		
+		System.out.println("__________________________________________________________________");
+		
+		System.out.println(student2.hashCode());
 	}
 
 }
